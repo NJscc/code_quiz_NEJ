babc5f6649e75a03c486d52ef867657234fb3fcb
Nicholas Jones <nej5000@gmail.com>
2022-06-12 00:44:36 -0700

    final upload for functionality

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
-# code_quiz_NEJ
+# Code_Quiz_NEJ
 -Description-
 
+    This application gives the user a short quiz on some
 
-    Notes on Flaws:
-    - The header 'scores' button and timer are not hidden on the 'highscores' page.
-    - Despite hiding question lists, they are still given space in the webpage.
-    - Elements do not all scale well when the page is resized.
-    - Highscores page does not display elements by score value.
+    basic CSS and Javascript essentials. It then allows the user
+    
+    to store their username and score, which will be saved after they close the window.
+
+-Main Technology-
+
+    - Javascript
+
+-Collaborators-
+
+    - Nicholas Jones
 
 -Link-
 
@@ -14,16 +21,4 @@ https://njscc.github.io/code_quiz_NEJ/
 
 -Screenshot-
 
-Title of app
-
-	* Live link to deployed app
-
-	* List of technologies used
-
-	* 1-2 sentences explaining what the app is
-
-	* 2-3 Screenshots / gifs of the app
-
-	* License (usually MIT)
-	
-	* Contact information for you and any collaborators
+![Sample of image of score screen with elements added.](./assets/image.png)
--- script.js
+++ script.js
@@ -1,58 +1,56 @@
-// Starting page - 'coding quiz challenge' + description
-// ALL PAGES - 'View highscores' and timer in the top corners
-//  timer only starts once quiz has begun, and counts down
-// From there - display each question in turn, let user know
-//  when their answer was right or wrong
-//If right, move to next question
-//End - score=current time
-//User inputs name/initials for high score/submit button
-//submitting score takes you to high score page
-//high score page has buttons to clear all scores, or return to quiz
-
-//JS
-//  Have master variable controlling page displayed
-// CSS prebuilt classes will display values according to code
-//  there, but JS will add or remove items from main as needed
-
+var arrayScores = [];
 var page = 0;
 var timer = 90;
+var storeScore = 0;
+var scorePage = document.getElementById('scoreShortcut');
 var questionText = document.getElementById("title");
 var sectionForList = document.getElementById("mainDisplay");
 var secondText = document.getElementById("fillerText");
 var textReply = document.getElementById("subtext");
 var clockQuiz = document.getElementById('timer');
-var userName = "";
 var beginButton = document.getElementById('start');
 var firstQ = document.getElementById('Q1');
 var secondQ = document.getElementById('Q2');
 var thirdQ = document.getElementById('Q3');
 var fourthQ = document.getElementById('Q4');
 var fifthQ = document.getElementById('Q5');
+var submitUsernameButton = document.getElementById('submitName');
+var nameInput = document.getElementById('nameInput');
+var scoreBlock = document.getElementById("highscore");
+var returnFromScores = document.getElementById('restart');
+var submission = document.getElementById("submit");
+var reset = document.getElementById('clear');
+var scoresList = document.getElementById('scoreDisplayList');
 
-//var h1El = document.createElement("h1");
-//h1El.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+//initialize local storage
 
-//Main display must contain start button on page load
-//  on click for start - begin quiz, start timer - use css to hide main display - set attribute queryselector
-//  Easier to show/hide div than elements whithin it
+
 
 //timer interval function
 function startTimer() {
     var timerInterval = setInterval(function () {
-        if (timer > 1) {
+        // if start page, score/username input page, or highscore page
+        if ((page === 6) || (page === 7) || (page === 0)) {
+            timer = 90;
+            clockQuiz.textContent = timer;
+            clearInterval(timerInterval);
+        } else if (timer > 0 ){
             clockQuiz.textContent = timer + ' second(s) remaining';
             timer--;
+        } else if (timer < 0) {
+        //if it's not one of those pages, and the time has run out.
         } else {
-            clockQuiz.textContent = timer + ' second(s) remaining';
+            clockQuiz.textContent = 'Out of time! (0 points)';
             clearInterval(timerInterval);
+            page = 0;
+            redesignPage();
         }
+
     }, 1000);
 }
 
-
-
 //display 'incorrect' when the wrong button is pressed
-var wrongButtons = document.querySelectorAll('incorrectButton');
+var wrongButtons = document.querySelectorAll('button.incorrectButton');
 for (let i = 0; i < wrongButtons.length; i++) {
     var button = wrongButtons[i];
     button.addEventListener("click", answerWrong);
@@ -60,11 +58,12 @@ for (let i = 0; i < wrongButtons.length; i++) {
 
 function answerWrong() {
     textReply.textContent = 'Incorrect';
+    timer -= 10;
 }
 
 //display 'correct', take user to next question
 
-var rightButtons = document.querySelectorAll('correctButton');
+var rightButtons = document.querySelectorAll('button.correctButton');
 for (let i = 0; i < rightButtons.length; i++) {
     var button = rightButtons[i];
     button.addEventListener("click", answerRight);
@@ -74,6 +73,7 @@ for (let i = 0; i < rightButtons.length; i++) {
 function answerRight() {
     textReply.textContent = 'Correct';
     page = page + 1;
+    redesignPage();
 }
 
 //add on-click function to quiz start button
@@ -84,19 +84,51 @@ function initiateQuiz() {
     startTimer();
     beginButton.setAttribute('style', 'display: none');
 }
-beginButton.addEventListener("click", initiateQuiz);
+
 
 
 //Initial Setup
 questionText.textContent = "Code Quiz";
 secondText.textContent = "This is a short quiz on some simple code concepts. Press the button below to begin."
 clockQuiz.textContent = timer;
+beginButton.addEventListener("click", initiateQuiz);
+returnFromScores.addEventListener("click", function(event) {
+    event.preventDefault();
+    page = 0;
+    redesignPage();
+    scoreBlock.setAttribute('style', 'display: none');
+});
+
+//if there are scores, reset object to null
+//if there are no scores, do nothing
+reset.addEventListener('click', function(event) {
+    event.preventDefault();
+    var listItems = JSON.parse(localStorage.getItem('scoreObject'));
+    if (listItems !== null) {
+        localStorage.setItem('scoreObject', null);
+        arrayScores = [];
+        redesignPage()
+    } else {
+        return;
+    }
+});
+
+//highscore button
+scorePage.addEventListener('click', function(event) {
+    event.preventDefault();
+    beginButton.setAttribute('style', 'display: none');
+    page = 7;
+    redesignPage();
+})
 
 
-// Try to make webpage gets reformatted on button click
+
+
+// Try to make sure webpage gets reformatted on button click
 function redesignPage () {
     //Title Page
     if (page === 0) {
+        timer = 90;
       questionText.textContent = "Code Quiz";
       secondText.textContent = "This is a short quiz on some simple code concepts. Press the button below to begin."
       clockQuiz.textContent = timer;
@@ -107,14 +139,14 @@ function redesignPage () {
     if (page === 1) {
       questionText.textContent = "Which character is used to denote custom classes in a CSS file?";
       secondText.textContent = "";
-      firstQ.setAttribute('style', 'visibility: visible');
+      firstQ.setAttribute('style', 'display: block');
     }
 
 //Q2
     if (page === 2) {
         questionText.textContent = "Q2";
-        var list = document.getElementById("Q2");
-        list.setAttribute('display', 'visible');
+        firstQ.setAttribute('style', 'display: none');
+        secondQ.setAttribute('style', 'display: block');
 
         questionText.textContent = 'Which character is used to denote an ID in a CSS file?';
     }
@@ -122,17 +154,17 @@ function redesignPage () {
 //Q3
     if (page === 3) {
       questionText.textContent = "Q3";
-      var list = document.getElementById("Q3");
-      list.setAttribute('display', 'visible');
+      secondQ.setAttribute('style', 'display: none');
+      thirdQ.setAttribute('style', 'display: block');
 
-       questionText.textContent = 'In JavaScript, of the options below, what is the best code element for accessing elements of an array?';
+      questionText.textContent = 'In JavaScript, of the options below, what is the best code element for accessing elements of an array?';
     }
 
 //Q4
     if (page === 4) {
         questionText.textContent = "Q4";
-        var list = document.getElementById("Q4");
-        list.setAttribute('display', 'visible');
+        thirdQ.setAttribute('style', 'display: none');
+        fourthQ.setAttribute('style', 'display: block');
 
         questionText.textContent = 'Which html element below would be considered a semantic tag?';
     }
@@ -140,22 +172,24 @@ function redesignPage () {
 //Q5
     if (page === 5) {
         questionText.textContent = "Q5";
-        var list = document.getElementById("Q5");
-        list.setAttribute('display', 'visible');
+        fourthQ.setAttribute('style', 'display: none');
+        fifthQ.setAttribute('style', 'display: block');
 
         questionText.textContent = 'Which kind of brackets are used to access individual elements of an array?';
-        //button must end timer
-        clearInterval(timerInterval);
     }
 
 //Score and submit
     if (page === 6) {
          //localStorage.setItem("name", value);
+        storeScore = timer;
+        startTimer();
+        fifthQ.setAttribute('style', 'display: none');
         questionText.textContent = "Quiz Complete!";
-        secondText.textContent = "Your score was" ;
+        secondText.textContent = "Your score was " + timer + " points.";
+        textReply.textContent = '';
 
-        var submission = document.getElementById("submit");
-        submission.setAttribute('display', 'visible');
+
+        submission.setAttribute('style', 'display: block');
         //add text for current score
         //submission box
         //automatically takes user to highscore page when submitted
@@ -164,24 +198,60 @@ function redesignPage () {
 
     //Highscore page
     if (page === 7) {
+        submission.setAttribute('style', 'display: none');
+        fifthQ.setAttribute('style', 'display: none');
+        fourthQ.setAttribute('style', 'display: none');
+        thirdQ.setAttribute('style', 'display: none');
+        secondQ.setAttribute('style', 'display: none');
+        firstQ.setAttribute('style', 'display: none');
+        textReply.textContent = '';
         questionText.textContent = "Highscores";
         secondText.textContent = "";
-        var scoreList = document.getElementById("highscore");
-        scoreList.setAttribute('display', 'visible');
-    
-        //localStorage.getItem("name");
-        //buttons clear local storage, go to start
+        scoreBlock.setAttribute('style', 'display: block');
+
+        //reset ul on this page
+        scoresList.innerHTML = "";
+       
+        var values = JSON.parse(localStorage.getItem('scoreObject'));
+        //if there are no scores
+        if (values == null) {
+            var newItem = document.createElement("p")
+            scoresList.append(newItem);
+            newItem.textContent = "No Scores to Display"
+        } else {
+            for (const property in values) {
+                var newElement = document.createElement("p");
+                scoresList.append(newElement)
+                newElement.textContent = `${values[property]}`;
+            }
+        }
 
     }
 
 }
 
 
+//button to submit localstorage data
+submitUsernameButton.addEventListener("click", function(event) {
+    event.preventDefault();
 
 
-//begin timer
-function startQuiz() {
+    //object from parsing local storage
+    var scores = JSON.parse(localStorage.getItem("scoreObject"));
 
-}
+    var username = nameInput.value;
+    //score - saved as 'storeScore' outside this function
+    arrayScores.push(username + ' : ' + storeScore);
 
 
+    if (username === "") {
+        secondText.textContent = "Please give at least one character."
+    //if localstorage value is null
+    } else {
+        localStorage.clear();
+        var storeString = JSON.stringify(arrayScores);
+        localStorage.setItem("scoreObject", storeString);
+        page = 7;
+        redesignPage();
+    }
+});
